81b4ae810195fac222959fcd90d5c1bb1c1ee211
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-04 23:40:58 +0200

    Update Ejercicio6.py

---
+++ Ejercicio6.py
@@ -0,0 +1,7 @@
+n= int(input('Introduzca un número entero positivo'))
+
+if n>0 :
+    suma=((n*(n+1))/2)
+    print(suma)
+else :
+    print('El valor introducido no es un número positivo, introduzca un número positivo')
