502b7537a935ac2fa315c7884039f48e410e08c2
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-04 17:04:46 +0200

    Update Ejercicio5.py

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,6 +1,4 @@
-horas=input('Indique el número de horas trabajadas')
-coste=input('Indique el coste por hora de trabajo')
-horas=x
-coste=y
-x= x*y
+horas= int(input('Indique el número de horas trabajadas'))
+coste=int(input('Indique el coste por hora de trabajo'))
+x= horas*coste
 print('A usted le corresponde un pego de',x,'euros')
