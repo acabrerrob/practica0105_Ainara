379df91ce212a725faf24b3c181557abdecd0744
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-05 16:04:02 +0200

    Update Ejercicio9.py

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -1,3 +1,9 @@
-cantidad= int(input('Introduzca la cantidad de capital a invertir'))
-interes= int(input('Introduzca el porcentaje de interes anual'))
-años= int(input('Introduzca el número de años que quiere que dure su inersión'))
+cantidad= int (input('Introduzca la cantidad de capital a invertir'))
+interes= int (input('Introduzca el porcentaje de interes anual'))
+años= int (input('Introduzca el número de años que quiere que dure su inersión'))
+
+while años>0:
+    capital=(cantidad+((interes*cantidad)/100))
+    cantidad=capital
+    años=años-1
+    print(capital)
