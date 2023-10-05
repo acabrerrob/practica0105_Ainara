4a9f1de529258ebe53d7556994ea2f82562d5ea0
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-05 15:24:05 +0200

    Update Ejercicio8.py

---
+++ Ejercicio8.py
@@ -0,0 +1,5 @@
+n= int(input('Introduzca el primer número entero'))
+m= int(input('Introduzca el segundo número entero'))
+c=(n//m)
+r=(n/m)-c
+print('El número',n,'entre',m,'da un cociente',c,'y un resto',r)
