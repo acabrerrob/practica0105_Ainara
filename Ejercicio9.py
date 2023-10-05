732ece891e598d698202806145eacecc095d63aa
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-05 16:23:03 +0200

    Update Ejercicio9.py

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -2,8 +2,9 @@ cantidad= int (input('Introduzca la cantidad de capital a invertir'))
 interes= int (input('Introduzca el porcentaje de interes anual'))
 años= int (input('Introduzca el número de años que quiere que dure su inersión'))
 
-while años>0:
+while años>0  :
     capital=(cantidad+((interes*cantidad)/100))
+    print('Su capital obtenido en el año',años,'es de:',capital)
     cantidad=capital
     años=años-1
-    print(capital)
+    
