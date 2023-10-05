621108074b378d32ef840065d49d3b800510fa2c
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-05 15:59:15 +0200

    Update Ejercicio9.py

---
+++ Ejercicio9.py
@@ -0,0 +1,3 @@
+cantidad= int(input('Introduzca la cantidad de capital a invertir'))
+interes= int(input('Introduzca el porcentaje de interes anual'))
+años= int(input('Introduzca el número de años que quiere que dure su inersión'))
